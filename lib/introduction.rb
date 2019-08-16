f449e6cbd118487094a6d4b1cdd7e477fa70b870
Jason Hundal <jasonhundal23@gmail.com>
2019-08-16 23:49:19 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,5 +2,5 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 def introduction_with_language("Dan", "Ember.js")).to out
-  puts ("Hi, my name is Dan)
+  puts ("Hi, my name is "Dan")
 end
