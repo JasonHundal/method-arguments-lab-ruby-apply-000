7031f8cec9f0c7a43b067b59d6c74c630a8851fb
Jason Hundal <jasonhundal23@gmail.com>
2019-08-16 23:29:58 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,4 +2,5 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 def introduction_with_languageO(name)
-  puts ""
+  puts "Hi, my name is Dan and I am learning to program in Ember.js.\n"
+end
